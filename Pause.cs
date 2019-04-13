53a53657e23a01aed3c66724a747c79e42b06e6d
brandonfleming <41931266+brandonfleming@users.noreply.github.com>
2019-04-13 16:34:30 -0500

    added Pause Menu

---
+++ Pause.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [Header("Player")]
+    public GameObject Player;
+    public GameObject Weapons;
+    [Header("UI")]
+    public GameObject Menu;
+
+    public void Pausee ()
+    {
+
+        Time.timeScale = 0f;
+        Menu.SetActive(true);
+        Player.GetComponent<FirstPersonController>().enabled = false;
+        Weapons.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Debug.Log("Paused");
+
+    }
+
+    public void UnPause ()
+    {
+        Time.timeScale = 1f;
+        Menu.SetActive(false);
+        Player.GetComponent<FirstPersonController>().enabled = true;
+        Weapons.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Debug.Log("Resumed Game");
+    }
+
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pausee();
+        }
+
+    }
+
+
+}
